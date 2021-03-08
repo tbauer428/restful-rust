4cb44b2f5942ee4391654b10d5ab92dbf14ea2c2
tbauer1738 <tom.bauer95@gmail.com>
2021-03-07 19:20:27 -0600

    added some basic endpoints, playing with args

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,32 @@
 #![feature(proc_macro_hygiene, decl_macro)]
-
 #[macro_use] extern crate rocket;
 
-#[get("/hello")]
+use rocket::http::{RawStr, Status};
+
+#[get("/")]
 fn index() -> &'static str {
     "Hello, world!"
 }
 
-fn main() {
-    rocket::ignite().mount("/", routes![index]).launch();
+#[get("/hello/<name>")]
+fn hello_name(name: &RawStr) -> String {
+    format!("Hello, {}!", name.as_str())
 }
 
+#[get("/fail")]
+fn just_fail() -> Status {
+    Status::NotAcceptable
+}
 
+#[get("/hello/<name>/<age>/<cool>")]
+fn hello_cool(name: String, age: u8, cool: bool) -> String {
+    if cool {
+        format!("You're a cool {} year old, {}!", age, name)
+    } else {
+        format!("{}, we need to talk about your coolness.", name)
+    }
+}
+
+fn main() {
+    rocket::ignite().mount("/", routes![index, hello_name, just_fail, hello_cool]).launch();
+}
